6ebad3c89cb54597dc9c6dead78e1c12ab9474a0
joshua williams <joshua@joshuas-MBP.fios-router.home>
2020-05-22 11:52:26 -0400

    login form is functional. need to handle storing token to localStorage and redirection to PR component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
+const initFormValue = {
+  username: "Lambda School",
+  password: "i<3Lambd4",
+};
 const Login = () => {
+  const [credentials, setCredentials] = useState(initFormValue);
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const changeHandle = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/api/login", credentials)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((er) => {
+        console.log(er);
+      });
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div>
+        <form onSubmit={submitHandle}>
+          <input
+            name="username"
+            type="text"
+            value={credentials.username}
+            onChange={changeHandle}
+            placeholder="Username"
+          />
+          <input
+            name="password"
+            type="password"
+            value={credentials.password}
+            onChange={changeHandle}
+            placeholder="Password"
+          />
+          <button>Submit</button>
+        </form>
+      </div>
     </>
   );
 };
---
+++ client/src/utils/axiosWithAuth.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+
+export const axiosWithAuth = () => {
+  const token = window.localStorage.getItem("token");
+  return axios.create({
+    headers: {
+      Authorization: token,
+    },
+    baseURL: "http://localhost:5000",
+  });
+};
